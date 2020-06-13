7e24a67cddc8a070f5a0d97e620c0bb089449219
Darrell-Spurs <darrellyu0413@gmail.com>
2020-06-13 12:52:08 +0800

    06131205

--- One.py
+++ One.py
@@ -1,1 +1,11 @@
-print("Remote Branch")
+def name2key(name):
+    ind = name.find(" ")
+    while ind!=-1:
+        name = name[:ind]+"+"+name[ind+1:]
+        ind = name.find(" ")
+    return name
+
+player = "Donny van de beek"
+searchkey = name2key(player)
+url="https://www.transfermarkt.com/schnellsuche/ergebnis/schnellsuche?query=%s&x=0&y=0"%searchkey
+print(url)
